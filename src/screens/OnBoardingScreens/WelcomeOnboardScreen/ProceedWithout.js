77d1e8bd384f2f3197d6c0300714783acaea67d5
sumitsabhaya <sumitsabhaya2001@gmail.com>
2024-06-12 12:14:54 +0530

    sumit

--- src/screens/OnBoardingScreens/WelcomeOnboardScreen/ProceedWithout.js
+++ src/screens/OnBoardingScreens/WelcomeOnboardScreen/ProceedWithout.js
@@ -11,16 +11,14 @@ const ProceedWithout = () => {
     const theme = useSelector(state => state.ThemeReducer);
     const COLOR = theme == 1 ? COLOR_DARK : COLOR_LIGHT;
     return (
-        <View style={{ width: Screen_Width, height: Screen_Height, paddingHorizontal: 15, paddingVertical: 10, backgroundColor:COLOR.WHITE,justifyContent:'center',alignItems:'center'}}>
-            
-                <Text style={{ color: COLOR.BLACK, fontSize: 30, fontWeight: '800' }}>Freshr</Text>
-           
-                <TouchableOpacity onPress={() => navigation.navigate(NavigationScreens.SignUpScreen)} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',width:Screen_Width*0.9,height:50,position:'absolute',bottom:150,backgroundColor:COLOR.ChartBlue,borderRadius:25,marginHorizontal:20}}>
-                    <Text style={{ color: COLOR.WHITE, fontSize: 18, fontWeight: '500' }}>Sign up / Sign in</Text>
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate(NavigationScreens.HomeTab)} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center',width:Screen_Width*0.9,height:50,position:'absolute',bottom:80,backgroundColor:COLOR.ORANGECOLOR,borderRadius:25,marginHorizontal:20}}>
-                    <Text style={{ color: COLOR.WHITE, fontSize: 18, fontWeight: '500' }}>Proceed without</Text>
-                </TouchableOpacity>
+        <View style={{ width: Screen_Width, height: Screen_Height, paddingHorizontal: 15, paddingVertical: 10, backgroundColor: COLOR.WHITE, justifyContent: 'center', alignItems: 'center' }}>
+            <Text style={{ color: COLOR.BLACK, fontSize: 30, fontWeight: '800' }}>Freshr</Text>
+            <TouchableOpacity onPress={() => navigation.navigate(NavigationScreens.SignUpScreen)} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: Screen_Width * 0.9, height: 50, position: 'absolute', bottom: 150, backgroundColor: COLOR.ChartBlue, borderRadius: 25, marginHorizontal: 20 }}>
+                <Text style={{ color: COLOR.WHITE, fontSize: 18, fontWeight: '500' }}>Sign up / Sign in</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => navigation.navigate(NavigationScreens.HomeTab)} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', width: Screen_Width * 0.9, height: 50, position: 'absolute', bottom: 80, backgroundColor: COLOR.ORANGECOLOR, borderRadius: 25, marginHorizontal: 20 }}>
+                <Text style={{ color: COLOR.WHITE, fontSize: 18, fontWeight: '500' }}>Proceed without</Text>
+            </TouchableOpacity>
         </View>
     )
 }
