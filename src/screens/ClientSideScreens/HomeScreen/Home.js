0908de860e0385652077ecd5e025f88219c39ea4
sumitsabhaya <sumitsabhaya2001@gmail.com>
2024-06-20 16:58:18 +0530

    sumit

--- src/screens/ClientSideScreens/HomeScreen/Home.js
+++ src/screens/ClientSideScreens/HomeScreen/Home.js
@@ -231,6 +231,7 @@ import { BASE_API_URL } from '../../../Services';
 import axios from 'axios';
 import Geolocation from 'react-native-geolocation-service';
 import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
+import RBSheet from 'react-native-raw-bottom-sheet';
 
 const Home = () => {
   const [currentPage, setCurrentPage] = useState(0);
@@ -241,6 +242,13 @@ const Home = () => {
   const [error, setError] = useState(null);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [address, setAddress] = useState({
+    Address: '',
+    city: '',
+    state: '',
+    Nearbylandmark: ''
+  });
+  const refRBSheet = useRef(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -322,8 +330,7 @@ const Home = () => {
   const COLOR = theme == 1 ? COLOR_DARK : COLOR_LIGHT;
   const COLOR1 = theme == 1 ? GRADIENT_COLOR_DARK : GRADIENT_COLOR_LIGHT;
   const [activeTab, setActiveTab] = useState('Delivery');
-  const [isLocationNameFocused, setIsLocationNameFocused] = useState(false);
-  const [locationName, setLocationName] = useState('mumbai');
+
 
   const currentHour = new Date().getHours();
   let greeting;
@@ -337,14 +344,28 @@ const Home = () => {
   }
 
   const styles = StyleSheet.create({
-    input: {
-      marginLeft: 5,
-      color: COLOR.BLACK,
-      width: 100,
-      fontSize: 16,
-      height: 38,
-    },
   });
+
+  const openBottomSheet = () => {
+    refRBSheet.current.open();
+  };
+
+  const closeBottomSheet = () => {
+    refRBSheet.current.close();
+  };
+
+  const handleSaveAddress = () => {
+    refRBSheet.current.close();
+  };
+
+  const formatAddress = (address) => {
+    const fullAddress = `${address.Address}, ${address.city}, ${address.state}, ${address.Nearbylandmark}`;
+    if (fullAddress.length > 15) {
+      return `${fullAddress.substring(0, 15)}...`;
+    }
+    return fullAddress;
+  };
+
 
   return (
     <ScrollView style={{ width: Screen_Width, height: Screen_Height, paddingHorizontal: 15, backgroundColor: COLOR.WHITE }}>
@@ -352,17 +373,12 @@ const Home = () => {
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <View>
             <Text style={{ color: COLOR.BLACK, fontSize: 18 }}>{greeting}, Manav</Text>
-            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+            <TouchableOpacity onPress={openBottomSheet} style={{ flexDirection: 'row', alignItems: 'center', marginLeft: 5, width: 150, height: 38 }}>
               <Entypo name="location-pin" size={15} color={COLOR.BLACK} />
-              <TextInput
-                style={[styles.input, { color: COLOR.BLACK }]}
-                placeholderTextColor={COLOR.BLACK}
-                onFocus={() => setIsLocationNameFocused(true)}
-                onBlur={() => setIsLocationNameFocused(false)}
-                value={locationName}
-                onChangeText={text => setLocationName(text)}
-              />
-            </View>
+              <Text style={{ color: COLOR.BLACK, fontSize: 16 }}>
+                {formatAddress(address)}
+              </Text>
+            </TouchableOpacity>
           </View>
         </View>
         <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
@@ -447,6 +463,126 @@ const Home = () => {
         <Category />
       </View>
 
+
+      <RBSheet
+        ref={refRBSheet}
+        height={Screen_Height * 0.80}
+        customStyles={{
+          wrapper: {
+            backgroundColor: COLOR.BLACK_40,
+          },
+          container: {
+            backgroundColor: COLOR.WHITE,
+            borderRadius: 40,
+            borderBottomRightRadius: 0,
+            borderBottomLeftRadius: 0,
+            elevation: 10,
+            shadowColor: COLOR.BLACK,
+          },
+          draggableIcon: {
+            backgroundColor: COLOR.BLACK,
+          },
+        }}
+        customModalProps={{
+          animationType: 'slide',
+          statusBarTranslucent: true,
+        }}
+        customAvoidingViewProps={{
+          enabled: false,
+        }}
+      >
+        <ScrollView
+          style={{
+            width: Screen_Width,
+            height: 100,
+            paddingHorizontal: 15,
+            backgroundColor: COLOR.WHITE,
+          }}
+        >
+          
+          <View style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginVertical: 15,
+              justifyContent:'space-between',
+              margin:10
+            }}>
+          <Text style={{ fontSize: 20, color: COLOR.BLACK }}>Add a Location</Text>
+            <TouchableOpacity onPress={closeBottomSheet}><AntDesign name="closecircleo" size={25} color={COLOR.BLACK} /></TouchableOpacity> 
+          </View>
+          <View style={{margin:10}}>
+            <TextInput
+              placeholder="Address"
+              value={address.Address}
+              onChangeText={(text) => setAddress({ ...address, Address: text })}
+              style={{
+                borderWidth: 1,
+                borderColor: COLOR.GRAY,
+                borderRadius: 5,
+                padding: 10,
+                marginBottom: 10,
+                backgroundColor: COLOR.WHITE,
+              }}
+            />
+            <TextInput
+              placeholder="City"
+              value={address.city}
+              onChangeText={(text) => setAddress({ ...address, city: text })}
+              style={{
+                borderWidth: 1,
+                borderColor: COLOR.BLACK,
+                borderRadius: 5,
+                padding: 10,
+                marginBottom: 10,
+                backgroundColor: COLOR.WHITE,
+              }}
+            />
+            <TextInput
+              placeholder="State"
+              value={address.state}
+              onChangeText={(text) => setAddress({ ...address, state: text })}
+              style={{
+                borderWidth: 1,
+                borderColor: COLOR.GRAY,
+                borderRadius: 5,
+                padding: 10,
+                marginBottom: 10,
+                backgroundColor: COLOR.WHITE,
+              }}
+            />
+            <TextInput
+              placeholder="Nearbylandmark"
+              value={address.Nearbylandmark}
+              onChangeText={(text) => setAddress({ ...address, Nearbylandmark: text })}
+              style={{
+                borderWidth: 1,
+                borderColor: COLOR.GRAY,
+                borderRadius: 5,
+                padding: 10,
+                marginBottom: 10,
+                backgroundColor: COLOR.WHITE,
+              }}
+              keyboardType="numeric"
+            />
+          </View>
+          <TouchableOpacity
+            style={{
+              width: Screen_Width * 0.90,
+              height: Screen_Height * 0.05,
+              backgroundColor: COLOR.ORANGECOLOR,
+              justifyContent: 'center',
+              borderRadius: 35,
+              alignSelf: 'center',
+              marginVertical: 20
+            }}
+            onPress={handleSaveAddress}
+          >
+            <Text style={{ textAlign: 'center', fontSize: 16, color: COLOR.WHITE }}>
+              Save
+            </Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </RBSheet>
       <View style={{ height: 90 }} />
     </ScrollView>
   );
