15d369b5df0526b16991030cd25219c4156d80d8
Manav612 <123803394+Manav612@users.noreply.github.com>
2024-06-22 16:31:07 +0530

    manav

--- src/screens/ProfessionalScreens/ProfessionalServices/profselectedDetailservice.js
+++ src/screens/ProfessionalScreens/ProfessionalServices/profselectedDetailservice.js
@@ -1,7 +1,7 @@
 import { ScrollView, StyleSheet, Text, View, Image, TouchableOpacity, TextInput, FlatList, Alert } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { Screen_Height, Screen_Width } from '../../../constants/Constants';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { COLOR_DARK, COLOR_LIGHT, GRADIENT_COLOR_DARK, GRADIENT_COLOR_LIGHT } from '../../../constants/Colors';
 import { useNavigation } from '@react-navigation/native';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -11,19 +11,22 @@ import { NavigationScreens } from '../../../constants/Strings';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { BASE_API_URL } from '../../../Services';
 import axios from 'axios';
+import { UpdateServiceData } from '../../../redux/ServicesData/ServicesDataAction';
 
 const ProfSelectedDetailService = ({ route }) => {
-    const { item,data } = route.params;
+    const { item, data } = route.params;
     // console.log("=====   data   ========",data);
     const theme = useSelector(state => state.ThemeReducer);
     const COLOR = theme === 1 ? COLOR_DARK : COLOR_LIGHT;
     const COLOR1 = theme === 1 ? GRADIENT_COLOR_DARK : GRADIENT_COLOR_LIGHT;
     const navigation = useNavigation();
-    const [distance, setDistance] = useState(item.averageDuration);
+    const [distance, setDistance] = useState(item?.averageDuration);
     const [open, setOpen] = useState(false);
-    const [price, setPrice] = useState(item.averagePrice.toString());
+    const [price, setPrice] = useState(item?.averagePrice.toString());
     const [isLoading, setIsLoading] = useState(false);
     const [selectedGender, setSelectedGender] = useState('both');
+    const [description, setDescription] = useState(item?.description);
+    const dispatch = useDispatch();
 
 
     const handleSelect = (selectedItem) => {
@@ -45,35 +48,35 @@ const ProfSelectedDetailService = ({ route }) => {
 
     const RadioButton = ({ label, selected, onPress }) => (
         <TouchableOpacity style={{ flexDirection: 'row', alignItems: 'center', marginVertical: 5 }} onPress={onPress}>
-          <View style={{
-            height: 24,
-            width: 24,
-            borderRadius: 12,
-            borderWidth: 2,
-            borderColor: COLOR.BLACK,
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginRight: 10,
-          }}>
-            {selected ? 
-              <View style={{
-                height: 12,
-                width: 12,
-                borderRadius: 6,
-                backgroundColor: COLOR.BLACK,
-              }}/>
-              : null
-            }
-          </View>
-          <Text style={{ color: COLOR.BLACK, fontSize: 16 }}>{label}</Text>
+            <View style={{
+                height: 24,
+                width: 24,
+                borderRadius: 12,
+                borderWidth: 2,
+                borderColor: COLOR.BLACK,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginRight: 10,
+            }}>
+                {selected ?
+                    <View style={{
+                        height: 12,
+                        width: 12,
+                        borderRadius: 6,
+                        backgroundColor: COLOR.BLACK,
+                    }} />
+                    : null
+                }
+            </View>
+            <Text style={{ color: COLOR.BLACK, fontSize: 16 }}>{label}</Text>
         </TouchableOpacity>
-      );
-      const handleCreate = async () => {
+    );
+    const handleCreate = async () => {
         if (!price || parseFloat(price) < item.minimumPrice) {
             Alert.alert("Invalid Price", "Please enter a valid price.");
             return;
         }
-    
+
         setIsLoading(true);
         try {
             const token = await AsyncStorage.getItem("AuthToken");
@@ -83,23 +86,26 @@ const ProfSelectedDetailService = ({ route }) => {
                     'Content-Type': 'application/json'
                 }
             };
-            
+
             const serviceData = {
                 name: item.name,
                 photo: item.photo,
                 price: parseFloat(price),
                 averageDuration: distance,
-                description: item.description,
+                description: description,
                 serviceType: item._id,
                 forMasculine: selectedGender === 'masculine' || selectedGender === 'both',
                 forFeminine: selectedGender === 'feminine' || selectedGender === 'both'
             };
-    
+
             const res = await axios.post(`${BASE_API_URL}/professionals/professional/services`, serviceData, config);
-            console.log("Service created successfully:", res.data.data);
-            
+            if (res && res.data) {
+                console.log("Service created successfully:", res.data.data.service);
+                dispatch(UpdateServiceData(res.data.data.service));
+            }
+
             Alert.alert("Success", "Service created successfully");
-            navigation.goBack();
+            // navigation.goBack();
         } catch (error) {
             console.error("Error creating service:", error.response?.data || error.message);
             Alert.alert("Error", "Failed to create service. Please try again.");
@@ -110,21 +116,24 @@ const ProfSelectedDetailService = ({ route }) => {
 
     const styles = StyleSheet.create({
         input: {
-            width: Screen_Width * 0.89, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, borderColor: COLOR.BLACK, borderWidth: 1
+            width: Screen_Width * 0.89, height: 100, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, borderColor: COLOR.BLACK, borderWidth: 1, color: COLOR.BLACK
         },
         button: {
             backgroundColor: COLOR.ORANGECOLOR,
-            justifyContent: 'center',
+            justifyContent:'center',
             borderRadius: 20,
-            alignItems: 'center',
+           alignSelf:'center',
             height: 40,
             width: Screen_Width * 0.9,
             marginHorizontal: 10,
             marginVertical: 10,
+            position:'absolute',
+            bottom:80
         },
     });
 
     return (
+        <>
         <ScrollView style={{ width: Screen_Width, height: Screen_Height, paddingHorizontal: 15, backgroundColor: COLOR.WHITE }}>
             <View style={{ width: Screen_Width * 0.89, height: 60, borderRadius: 10, marginHorizontal: 10, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row' }}>
                 <TouchableOpacity onPress={() => navigation.goBack()}><AntDesign name="arrowleft" size={30} color={COLOR.GRAY} /></TouchableOpacity>
@@ -134,8 +143,8 @@ const ProfSelectedDetailService = ({ route }) => {
             </View>
             <TouchableOpacity onPress={() => setOpen(!open)} style={{ backgroundColor: COLOR.LIGHTGRAY, width: Screen_Width * 0.89, height: 60, borderRadius: 10, marginHorizontal: 10, alignItems: 'center', justifyContent: 'space-between', flexDirection: 'row', paddingHorizontal: 5 }}>
                 <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center', margin: 10 }}>
-                    <Image source={{ uri: item.photo }} style={{ height: 40, width: 40, resizeMode: 'cover', borderRadius: 10 }} />
-                    <Text style={{ fontSize: 16,color:COLOR.BLACK }}>{item.name}</Text>
+                    <Image source={{ uri: item?.photo }} style={{ height: 40, width: 40, resizeMode: 'cover', borderRadius: 10 }} />
+                    <Text style={{ fontSize: 16, color: COLOR.BLACK }}>{item?.name}</Text>
                 </View>
                 <AntDesign name={open ? "up" : "down"} size={27} color={COLOR.GRAY} />
             </TouchableOpacity>
@@ -147,19 +156,18 @@ const ProfSelectedDetailService = ({ route }) => {
                     renderItem={({ item: serviceItem }) => (
                         <TouchableOpacity onPress={() => handleSelect(serviceItem)} style={{ flexDirection: 'row', alignItems: 'center', gap: 10 }}>
                             <Image source={{ uri: serviceItem.photo }} style={{ height: 50, width: 50, resizeMode: 'cover', marginVertical: 10, borderRadius: 10 }} />
-                            <Text style={{ fontSize: 16,color:COLOR.BLACK }}>{serviceItem.name}</Text>
+                            <Text style={{ fontSize: 16, color: COLOR.BLACK }}>{serviceItem?.name}</Text>
                         </TouchableOpacity>
                     )}
                 />
             )}
-            <View style={{ backgroundColor: COLOR.LIGHTGRAY, width: Screen_Width * 0.89, height: 225, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, alignItems: 'center' }}>
-                <Image source={{ uri: item.photo }} style={{ height: 150, width: 150, resizeMode: 'cover', margin: 10 }} />
-                <TouchableOpacity style={{ backgroundColor: COLOR.ORANGECOLOR, height: 30, width: 125, borderRadius: 10, alignItems: 'center', justifyContent: 'center', marginVertical: 10 }}><Text style={{ color: COLOR.WHITE, fontSize: 16 }}>Change image</Text></TouchableOpacity>
+            <View style={{ backgroundColor: COLOR.LIGHTGRAY, width: Screen_Width * 0.89, height: 175, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, alignItems: 'center' }}>
+                <Image source={{ uri: item?.photo }} style={{ height: 150, width: 150, resizeMode: 'cover', margin: 10 }} />
             </View>
 
             <View style={{ backgroundColor: COLOR.LIGHTGRAY, width: Screen_Width * 0.89, height: 80, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, alignItems: 'center', justifyContent: 'center' }}>
                 <Text style={{ color: COLOR.ORANGECOLOR, fontSize: 18 }}>Prices</Text>
-                <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center'}}>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={{ color: COLOR.BLACK, fontSize: 18 }}>$</Text>
                     <TextInput
                         placeholderTextColor={COLOR.BLACK}
@@ -170,7 +178,7 @@ const ProfSelectedDetailService = ({ route }) => {
                     />
                 </View>
             </View>
-          
+
 
             <View style={{ width: Screen_Width * 0.89, height: 100, borderRadius: 10, marginHorizontal: 10, marginVertical: 10, borderColor: COLOR.BLACK, borderWidth: 1 }}>
                 <Text style={{ fontWeight: '700', color: COLOR.BLACK, fontSize: 14, margin: 10 }}>Estimated Max duration in Minutes : <Text style={{ color: COLOR.ORANGECOLOR }}>{distance} min</Text></Text>
@@ -187,35 +195,40 @@ const ProfSelectedDetailService = ({ route }) => {
                 />
             </View>
 
-               <TextInput
-               placeholder={item.description}
-               placeholderTextColor={COLOR.BLACK}
-               style={[styles.input,{color:COLOR.BLACK}]}
-               />
-            <View style={{ marginVertical: 10,flexDirection:'row',justifyContent:'space-between',alignItems:'center' }}>
-            <RadioButton 
-                label="Both"
-                selected={selectedGender === 'both'}
-                onPress={() => setSelectedGender('both')}
+            <TextInput
+                placeholder="Enter description"
+                placeholderTextColor={COLOR.GRAY}
+                style={[styles.input, { color: COLOR.BLACK, textAlignVertical: 'top', padding: 10 }]}
+                value={description}
+                onChangeText={setDescription}
+                multiline={true}
             />
-            <RadioButton 
-                label="Masculine"
-                selected={selectedGender === 'masculine'}
-                onPress={() => setSelectedGender('masculine')}
-            />
-            <RadioButton 
-                label="Feminine"
-                selected={selectedGender === 'feminine'}
-                onPress={() => setSelectedGender('feminine')}
-            />
-        </View>
-            <TouchableOpacity onPress={handleCreate} style={styles.button} disabled={isLoading}>
-                <Text style={{ color: COLOR.WHITE, fontSize: 16 }}>
-                    {isLoading ? "Creating..." : "Create Service"}
-                </Text>
-            </TouchableOpacity>
-            <View style={{ height: 90 }} />
+            <View style={{ marginVertical: 10, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                <RadioButton
+                    label="Both"
+                    selected={selectedGender === 'both'}
+                    onPress={() => setSelectedGender('both')}
+                />
+                <RadioButton
+                    label="Masculine"
+                    selected={selectedGender === 'masculine'}
+                    onPress={() => setSelectedGender('masculine')}
+                />
+                <RadioButton
+                    label="Feminine"
+                    selected={selectedGender === 'feminine'}
+                    onPress={() => setSelectedGender('feminine')}
+                />
+            </View>
+           
+            <View style={{ height: 160 }} />
         </ScrollView>
+         <TouchableOpacity onPress={handleCreate} style={styles.button} disabled={isLoading}>
+         <Text style={{ color: COLOR.WHITE, fontSize: 16,textAlign:'center' }}>
+             {isLoading ? "Creating..." : "Create Service"}
+         </Text>
+     </TouchableOpacity>
+     </>
     )
 }
 
